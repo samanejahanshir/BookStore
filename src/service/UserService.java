32848ac6d6dfb36e9bee325a6e24a03661f37eb2
unknown <jahanshir.sm92@gmail.com>
2021-11-07 02:03:03 -0800

    fix method showListBook in class Main

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -5,35 +5,51 @@ import model.Book;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class UserService {
-    BookDao bookDao=new BookDao();
+    BookDao bookDao = new BookDao();
 
     public UserService() throws SQLException, ClassNotFoundException {
     }
 
     public Map<String, List<Book>> showListBooks() throws SQLException, ClassNotFoundException {
-        List<Book> bookList=new ArrayList<>();
-        bookList=bookDao.getAll();
+        List<Book> bookList = new ArrayList<>();
+        bookList = bookDao.getAll();
         for (Book book : bookList) {
             book.setTotalSoldPrice(book.calculateTotalSoldPrice());
         }
-       Map<String,List<Book>> mapBooks= listBookByAuthor(bookList);
-       return  sortBookByYear(mapBooks);
-
+        Map<String, List<Book>> mapBooks = listBookByAuthor(bookList);
+        return sortBookByYear(mapBooks);
 
 
     }
+
     public Map<String, List<Book>> listBookByAuthor(List<Book> bookList) {
         return bookList.stream().collect(Collectors.groupingBy(i -> i.getAuthorName()));
     }
-    public Map<String, List<Book>> sortBookByYear(Map<String, List<Book>> mapBook){
-        mapBook.forEach((i,j)->{
-           j= j.stream().sorted((a,b)->b.getYear().compareTo(a.getYear())).collect(Collectors.toList());
+
+    public Map<String, List<Book>> sortBookByYear(Map<String, List<Book>> mapBook) {
+       /* mapBook.forEach((i,j)->{
+            j=mapBook.get(i);
+           j= j.stream().sorted((a,b)->Integer.compare(Integer.parseInt(b.getYear()),Integer.parseInt(a.getYear()))).collect(Collectors.toList());
         });
-        return mapBook;
+        return mapBook;*/
+        List<Book> books=new ArrayList<>();
+        Map<String, List<Book>> mapBookSorted =new HashMap<>();
+        //Function<String,List<Book>> function=(i)->mapBook.get(i);
+        for (Map.Entry<String, List<Book>> stringListEntry : mapBook.entrySet()) {
+            books = stringListEntry.getValue();
+            books.sort((a, b) -> b.getYear().compareTo(a.getYear()));
+            mapBookSorted.put(stringListEntry.getKey(), books);
+
+        }
+
+        return mapBookSorted;
     }
+
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -5,14 +5,11 @@ import model.Book;
 import service.UserService;
 
 import java.sql.SQLException;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-   static Scanner scanner=new Scanner(System.in);
-   static UserService userService;
+    static Scanner scanner = new Scanner(System.in);
+    static UserService userService;
 
     static {
         try {
@@ -31,24 +28,58 @@ public class Main {
             System.out.println("1.Show Books information\n2.exit");
 
             try {
-                switch (scanner.nextInt()){
+                switch (scanner.nextInt()) {
                     case 1:
                         showListBook();
-                        break ;
+                        break;
                     case 2:
                         System.out.println("---| GoodBye |---");
                         break outer;
                     default:
-                        throw  new InvalidInputException("Please Enter 1 or 2 !");
+                        throw new InvalidInputException("Please Enter 1 or 2 !");
                 }
-            }catch (InputMismatchException | SQLException | ClassNotFoundException e){
+            } catch (InputMismatchException | SQLException | ClassNotFoundException e) {
                 System.out.println(e.getMessage());
 
             }
         }
 
     }
+
     public static void showListBook() throws SQLException, ClassNotFoundException {
-        Map<String, List<Book>> books = userService.showListBooks();
-books.forEach((i,j)-> System.out.println(i+" "+ j));    }
+        Map<String, List<Book>> mapBooks = userService.showListBooks();
+        // mapBooks.forEach((i, j) -> System.out.println(i + " " + j));
+        System.out.format("+----------------------+-------------+-----------+---------------+------------+----------+------------------+%n");
+        System.out.format("|     Author Name      |     ISBN    |   Title   | PublisherYear |    Price   | SoldNum  | Sold Total Price |%n");
+        System.out.format("+----------------------+-------------+-----------+---------------+------------+----------+------------------+%n");
+        String leftAlignFormat = "| %-20s | %-11s | %-9s | %-13s | %-10d | %-8d | %-16d |%n";
+        List<Book> books = new ArrayList<>();
+        int length = 0;
+        for (Map.Entry<String, List<Book>> stringListEntry : mapBooks.entrySet()) {
+            books = stringListEntry.getValue();
+            String author = books.get(0).getAuthorName();
+            length++;
+            for (int i = 0; i < books.size(); i++) {
+
+                System.out.format(leftAlignFormat, author, books.get(i).getIsbn(), books.get(i).getTitle(), books.get(i).getYear(), books.get(i).getPrice()
+                        , books.get(i).getSoldNumber(), books.get(i).getTotalSoldPrice());
+
+                author = "";
+                if (i < books.size() - 1) {
+                    if (books.get(i).getAuthorName().equalsIgnoreCase(books.get(i + 1).getAuthorName())) {
+                        System.out.format("|                      |------------------------------------------------------------------------------------|%n");
+                    }
+                    if (!books.get(i).getAuthorName().equalsIgnoreCase(books.get(i + 1).getAuthorName())) {
+                        author = books.get(i + 1).getAuthorName();
+
+
+                    }
+                }
+            }
+            if (length!=mapBooks.size())
+                System.out.format("|-----------------------------------------------------------------------------------------------------------|%n");
+
+        }
+        System.out.format("+----------------------+-------------+-----------+---------------+------------+----------+------------------+%n");
+    }
 }
